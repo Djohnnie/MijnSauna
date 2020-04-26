1e409eaa501af70d8ad612266241de7021b94ce8
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2020-04-26 10:46:37 +0200

    Updated YAML for Middleware.Processor

---
+++ src/MijnSauna.Middleware.Tests/Controllers/GpioControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using MijnSauna.Middleware.Processor.Controllers;
+
+namespace MijnSauna.Middleware.Tests.Controllers
+{
+    public class GpioControllerTests
+    {
+        [Fact]
+        public async Task Repository_GetAll_Should_Return_All_Records()
+        {
+            // Act
+            var gpioController = new GpioController();
+
+            // Assert
+        }
+    }
+}
