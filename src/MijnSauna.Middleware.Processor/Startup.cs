a60707ee7bb8b0509de8cf2ca839d71e601c3ebd
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2020-04-26 11:08:23 +0200

    Testing CI

--- src/MijnSauna.Middleware.Processor/Startup.cs
+++ src/MijnSauna.Middleware.Processor/Startup.cs
@@ -11,8 +11,6 @@ namespace MijnSauna.Middleware.Processor
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.ConfigureProcessor();
@@ -22,7 +20,6 @@ namespace MijnSauna.Middleware.Processor
             services.AddHostedService<SampleWorker>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
