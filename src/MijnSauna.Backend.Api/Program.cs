6aba188cc5cc2598f6dddf4bdd7b2bb3754e49ad
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2020-05-15 16:12:02 +0200

    Made HTTPS certificate for MinSauna.Backend.Api optional

--- src/MijnSauna.Backend.Api/Program.cs
+++ src/MijnSauna.Backend.Api/Program.cs
@@ -24,8 +24,15 @@ namespace MijnSauna.Backend.Api
                     webBuilder.UseKestrel();
                     webBuilder.ConfigureKestrel((context, options) =>
                     {
-                        options.Listen(IPAddress.Any, 5000,
-                            listenOptions => { listenOptions.UseHttps(certificateFileName, certificatePassword); });
+                        if (string.IsNullOrEmpty(certificateFileName) || string.IsNullOrEmpty(certificatePassword))
+                        {
+                            options.Listen(IPAddress.Any, 5000);
+                        }
+                        else
+                        {
+                            options.Listen(IPAddress.Any, 5000,
+                                listenOptions => { listenOptions.UseHttps(certificateFileName, certificatePassword); });
+                        }
                     });
                     webBuilder.UseStartup<Startup>();
                 });
