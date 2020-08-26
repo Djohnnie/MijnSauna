afb175f255c45830a15fe43d4c1d7ccbce1b9f8a
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2020-08-27 00:04:22 +0200

    Upgraded Entity Framework and .NET related NuGet packages to version 5 preview 8

--- src/MijnSauna.Middleware.Tests/Controllers/GpioControllerTests.cs
+++ src/MijnSauna.Middleware.Tests/Controllers/GpioControllerTests.cs
@@ -1,18 +1,7 @@
-﻿using System.Threading.Tasks;
-using Xunit;
-using MijnSauna.Middleware.Processor.Controllers;
-
-namespace MijnSauna.Middleware.Tests.Controllers
+﻿namespace MijnSauna.Middleware.Tests.Controllers
 {
     public class GpioControllerTests
     {
-        [Fact]
-        public async Task Repository_GetAll_Should_Return_All_Records()
-        {
-            // Act
-            var gpioController = new GpioController();
-
-            // Assert
-        }
+        
     }
 }
