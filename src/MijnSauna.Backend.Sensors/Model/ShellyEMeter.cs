f5f99f17211d939c831726a0faac3c2f5dc4fe90
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2021-05-20 22:34:56 +0200

    Updated JSON serialization property customization

--- src/MijnSauna.Backend.Sensors/Model/ShellyEMeter.cs
+++ src/MijnSauna.Backend.Sensors/Model/ShellyEMeter.cs
@@ -1,8 +1,13 @@
-﻿namespace MijnSauna.Backend.Sensors.Model
+﻿using System.Text.Json.Serialization;
+
+namespace MijnSauna.Backend.Sensors.Model
 {
     public class ShellyEMeter
     {
+        [JsonPropertyName("power")]
         public decimal Power { get; set; }
+
+        [JsonPropertyName("voltage")]
         public decimal Voltage { get; set; }
     }
 }
--- src/MijnSauna.Backend.Sensors/Model/ShellyWifi.cs
+++ src/MijnSauna.Backend.Sensors/Model/ShellyWifi.cs
@@ -1,9 +1,16 @@
-﻿namespace MijnSauna.Backend.Sensors.Model
+﻿using System.Text.Json.Serialization;
+
+namespace MijnSauna.Backend.Sensors.Model
 {
     public class ShellyWifi
     {
+        [JsonPropertyName("connected")]
         public bool Connected { get; set; }
+
+        [JsonPropertyName("ssid")]
         public string Ssid { get; set; }
+
+        [JsonPropertyName("ip")]
         public string Ip { get; set; }
     }
 }
