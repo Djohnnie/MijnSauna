93f5697a3b651499088061b9dfb5d34cc27643c6
Djohnnie <7031043+Djohnnie@users.noreply.github.com>
2025-09-08 13:17:21 +0200

    Updated MCP Server tools

--- src/MijnSauna.Backend.Api/Tools/MijnSaunaTools.cs
+++ src/MijnSauna.Backend.Api/Tools/MijnSaunaTools.cs
@@ -1,4 +1,5 @@
 ﻿using MijnSauna.Backend.Logic.Interfaces;
+using MijnSauna.Common.DataTransferObjects.Sessions;
 using ModelContextProtocol.Server;
 using System.ComponentModel;
 using System.Threading.Tasks;
@@ -9,13 +10,14 @@ namespace MijnSauna.Backend.Api.Tools;
 public class MijnSaunaTools
 {
     private readonly ISensorLogic _sensorLogic;
+    private readonly ISessionLogic _sessionLogic;
 
     public MijnSaunaTools(ISensorLogic sensorLogic)
     {
         _sensorLogic = sensorLogic;
     }
 
-    [McpServerTool]
+    [McpServerTool(Name = $"mijnsauna_{nameof(GetSaunaState)}", ReadOnly = true)]
     [Description("Gets the state of the sauna: Off, Infrared heating or Finnish sauna heating.")]
     public async Task<string> GetSaunaState()
     {
@@ -32,5 +34,62 @@ public class MijnSaunaTools
         }
 
         return "Off";
+    }
+
+    [McpServerTool(Name = $"mijnsauna_{nameof(GetSaunaTemperature)}", ReadOnly = true)]
+    [Description("Gets the temperature, in degrees Celcius, inside the sauna cabin.")]
+    public async Task<string> GetSaunaTemperature()
+    {
+        var state = await _sensorLogic.GetSaunaTemperature();
+
+        return $"{state.Temperature} °C";
+    }
+
+    [McpServerTool(Name = $"mijnsauna_{nameof(StartSaunaSession)}", Idempotent = true)]
+    [Description("Start a sauna session.")]
+    public async Task<string> StartSaunaSession()
+    {
+        var currentSession = await _sessionLogic.GetActiveSession();
+
+        if (currentSession is not null)
+        {
+            return $"There is {(currentSession.IsSauna ? "a sauna" : "an infrared")} session already active.";
+        }
+
+        await _sessionLogic.QuickStartSession(new QuickStartSessionRequest { IsSauna = true });
+
+        return "A sauna session was started.";
+    }
+
+    [McpServerTool(Name = $"mijnsauna_{nameof(StartInfraredSession)}", Idempotent = true)]
+    [Description("Start an infrared session.")]
+    public async Task<string> StartInfraredSession()
+    {
+        var currentSession = await _sessionLogic.GetActiveSession();
+
+        if (currentSession is not null)
+        {
+            return $"There is {(currentSession.IsSauna ? "a sauna" : "an infrared")} session already active.";
+        }
+
+        await _sessionLogic.QuickStartSession(new QuickStartSessionRequest { IsInfrared = true });
+
+        return "An infrared session was started.";
+    }
+
+    [McpServerTool(Name = $"mijnsauna_{nameof(SwitchSaunaOff)}", Idempotent = true)]
+    [Description("Switch off the sauna.")]
+    public async Task<string> SwitchSaunaOff()
+    {
+        var currentSession = await _sessionLogic.GetActiveSession();
+
+        if (currentSession is null)
+        {
+            return "There is no active session to switch off.";
+        }
+
+        await _sessionLogic.CancelSession(currentSession.SessionId);
+
+        return $"The current {(currentSession.IsSauna ? "sauna" : "infrared")} session was stopped.";
     }
 }
